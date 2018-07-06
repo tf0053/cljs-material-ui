9a529932e68847b7ce09572417c641140b2a0762
Stephen Way <way.stephen@gmail.com>
2018-07-06 14:28:56 -0700

    Updated repo for mui@1.2.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/stephenway/cljs-material-ui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cljsjs/material-ui "1.3.1-0"]
+  :dependencies [[cljsjs/material-ui "1.2.1-0"]
                  [cljsjs/react "16.3.0-0"]
                  [cljsjs/react-dom "16.3.0-0"]
                  [org.clojure/clojure "1.9.0-alpha14"]
